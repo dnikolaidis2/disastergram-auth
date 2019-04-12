8f887e91ac1debe2632c2a75ae76b846468202c9
Dimitris Nikolaidis <dnikolaidis4@gmail.com>
2019-04-13 02:36:56 +0300

    Added login

--- auth/auth.py
+++ auth/auth.py
@@ -1,68 +1,108 @@
-from flask import Blueprint, request, abort, Response
+from flask import Blueprint, request, abort, Response, current_app, jsonify
 
 from auth.models import UserSchema, User
 
 from auth import db
 
+from functools import wraps
+
+import jwt, time
+
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/user', methods=['POST', 'GET'])
-def user():
-    if request.method == 'POST':
-        # TODO: could you use marshal.dump to do some input testing?
-        # Abort if false format
-        if not request.is_json:
-            abort(400)
+def enforce_json():
+    def decorator(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            if not request.is_json:
+                abort(400)
 
-        # Abort if user already exists
-        if User.query.filter(User.username == request.json['username']).count() != 0:
-            abort(400)
+            return f(*args, **kwargs)
 
-        # 1. Creates new user according to request params
-        # 2. Add to db
-        # 3. Commit changes
-        new_user = User(username=request.json['username'], password=request.json['password'])
-        db.session.add(new_user)
-        db.session.commit()
+        return wrapped
+    return decorator
 
-        # Return code 201: 'Created'
-        return Response(status=201)
 
-    if request.method == 'GET':
+def require_auth():
+    def decorator(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            return f(*args, **kwargs)
 
-        user_schema = UserSchema(exclude=['id'])
-        # getuser = User.query.get(1)
+        return wrapped
+    return decorator
 
-        # return user_schema.jsonify(getuser)
-        # Find if user exists
 
-        reqd_user = request.args.get('username')
-        if reqd_user is None:
-            # Returns all usernames in a single string
-            # ...for debugging reasons
-            str = ''
-            user_list = User.query.all()
-            for user in user_list:
-                str = str + user.username + '  '
-            return str
-        else:
-            get_user = User.query.filter(User.username == reqd_user).one()
-            return user_schema.jsonify(get_user)
+@bp.route('/user', methods=['POST'])
+@enforce_json()
+def user_register():
+    # TODO: could you use marshal.dump to do some input testing?
 
-        # Maybe check if they have the required perms?
-        # User
-        # Return user. Redirecting to its profile page
-        # return
+    # Abort if user already exists
+    if User.query.filter(User.username == request.json['username']).count() != 0:
+        abort(400)
+
+    # 1. Creates new user according to request params
+    # 2. Add to db
+    # 3. Commit changes
+    new_user = User(username=request.json['username'], password=request.json['password'])
+    db.session.add(new_user)
+    db.session.commit()
+
+    # Return code 201: 'Created'
+    return Response(status=201)
+
+
+@bp.route('/user/<username>')
+def user_get(username):
+    # TODO authenticate user
+    user_schema = UserSchema(exclude=['id', 'password'])
+
+    if username == 'all':
+        # Returns all usernames in a single string
+        # ...for debugging reasons
+        str = ''
+        user_list = User.query.all()
+        for user in user_list:
+            str = str + user.username + '  '
+        return str
+    else:
+
+        get_user = User.query.filter(User.username == username).one()
+        return user_schema.jsonify(get_user)
+
+    # Maybe check if they have the required perms?
+    # User
+    # Return user. Redirecting to its profile page
+    # return
 
 
 @bp.route('/login', methods=['POST'])
+@enforce_json()
 def login():
-    if request.method == 'POST':
+    # TODO: could you use marshal.dump to do some input testing?
 
-        if not request.is_json:
-            abort(400)
+    user = User.query.filter(User.username == request.json['username']).one()
 
-        if User.query.filter(User.username == request.json['username']).count() != 0:
-            abort(400)
+    if user is None:
+        abort(400)
+
+    if not user.check_password(request.json['password']):
+        abort(400)
+
+    payload = {
+        'iss': 'auth_server',                               # TODO: WHO ARE WE
+        'sub': request.json['username'],
+        'exp': int(time.mktime(time.gmtime())) + 60*10,     # 10 minute token
+        'nbf': int(time.mktime(time.gmtime()))
+    }
+
+    token = jwt.encode(payload, current_app.config['SECRET_KEY'])
+
+    return jsonify(token=token)
+
+
+# refresh
+# logout

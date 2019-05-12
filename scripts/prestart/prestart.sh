4d5400ab105890f0da312940abb49e2f4ed49a06
Dimitris Nikolaidis <dnikolaidis4@gmail.com>
2019-05-12 21:31:52 +0300

    Updated prestart.sh

--- scripts/prestart/prestart.sh
+++ scripts/prestart/prestart.sh
@@ -3,8 +3,8 @@
 # chmod +x init_db.py
 # ./init_db.py
 
-# check if migrations folder exists
-if [[ ! -d "$FLASK_APP_MIGRATIONS" ]]; then
+# check if migrations folder is not empty
+if ! [ "$(find "$FLASK_APP_MIGRATIONS" -mindepth 1 -print -quit 2>/dev/null)" ]; then
 	flask db init
 fi
 
